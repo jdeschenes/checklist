17d4ec9b21370017bc4b46fa85096237dafefe5c
Jean-Mathieu Deschenes <jm@pinkbot.com>
2025-02-13 07:40:31 -0500

    added test cases

--- src/domain/todo.rs
+++ src/domain/todo.rs
@@ -40,3 +40,47 @@ impl AsRef<str> for TodoName {
         &self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use core::panic;
+
+    use crate::error::APIError;
+
+    use super::{TodoName, MAX_TODO_NAME_LENGTH};
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn name_is_ok() {
+        let test_cases = vec![
+            ("banana", "banana"),
+            ("  banana  ", "banana"),
+            ("1234", "1234"),
+        ];
+        for test_case in test_cases {
+            let result: Result<TodoName, _> = test_case.0.parse();
+            assert_ok!(&result);
+            assert_eq!(result.unwrap().as_ref(), test_case.1);
+        }
+    }
+
+    #[test]
+    fn name_is_not_ok() {
+        let test_cases = vec![
+            ("", "Name cannot be empty".to_string()),
+            ("    ", "Name cannot be empty".to_string()),
+            (
+                "123456789012345678901",
+                format!("Name is too long cannot exceed: {}", MAX_TODO_NAME_LENGTH),
+            ),
+        ];
+        for test_case in test_cases {
+            let result: Result<TodoName, _> = test_case.0.parse();
+            assert_err!(&result);
+            match result.unwrap_err() {
+                APIError::BadRequest(x) => assert_eq!(x, test_case.1),
+                x => panic!("Unexpected error: {}", x),
+            }
+        }
+    }
+}
