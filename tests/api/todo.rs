b741cdfaaae2447678b898dba2ad88982ff4cc87
Jean-Mathieu Deschenes <jm@pinkbot.com>
2025-02-14 14:48:22 -0500

    Restructured the tests

--- tests/api/todo.rs
+++ tests/api/todo.rs
@@ -1,46 +1,8 @@
-use std::future::IntoFuture;
-use std::sync::LazyLock;
-
 use reqwest::StatusCode;
-use secrecy::SecretBox;
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
-use sqlx::{postgres::PgPoolOptions, Connection, Executor, PgConnection, PgPool};
 
-use checklist::configuration::{get_configuration, DatabaseSettings};
-use checklist::run;
-use checklist::telemetry::{get_subscriber, init_subscriber};
-
-struct TestApp {
-    address: String,
-}
-
-static TRACING: LazyLock<()> = LazyLock::new(|| {
-    let default_filter_level = "debug".to_string();
-    let subscriber_name = "test".to_string();
-    if std::env::var("TEST_LOG").is_ok() {
-        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
-        init_subscriber(subscriber).expect("failed to initialize subscriber");
-    } else {
-        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
-        init_subscriber(subscriber).expect("failed to initialize subscriber");
-    };
-});
-
-#[tokio::test]
-async fn health_check_works() {
-    let test_app = spawn_app().await;
-    let address = test_app.address;
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(format!("{}/health_check", address))
-        .send()
-        .await
-        .expect("Failed to execute request");
-    assert_eq!(response.status(), StatusCode::OK);
-    assert_eq!(Some(0), response.content_length());
-}
+use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn create_todo_works() {
@@ -190,60 +152,3 @@ async fn get_todo() {
     assert_eq!(get_response.status(), StatusCode::NOT_FOUND);
 }
 
-pub async fn configure_database(configuration: &DatabaseSettings) -> PgPool {
-    let maintenance_settings = DatabaseSettings {
-        host: configuration.host.clone(),
-        port: configuration.port,
-        database: "postgres".to_string(),
-        user: "postgres".to_string(),
-        password: SecretBox::new(Box::new("password".to_string())),
-        max_connections: configuration.max_connections,
-        pool_acquire_timeout: configuration.pool_acquire_timeout,
-        require_ssl: false,
-    };
-    let mut connection = PgConnection::connect_with(&maintenance_settings.connection_options())
-        .await
-        .expect("Failed to connect to postgres");
-
-    connection
-        .execute(
-            format!(
-                r#"CREATE DATABASE "{}" WITH OWNER "{}";"#,
-                configuration.database, configuration.user
-            )
-            .as_str(),
-        )
-        .await
-        .expect("Unable to create database");
-
-    let pool = PgPoolOptions::new()
-        .max_connections(configuration.max_connections)
-        .acquire_timeout(configuration.pool_acquire_timeout)
-        .connect_lazy_with(configuration.connection_options());
-
-    sqlx::migrate!("./migrations")
-        .run(&pool)
-        .await
-        .expect("Failed to migrate database");
-    pool
-}
-
-async fn spawn_app() -> TestApp {
-    LazyLock::force(&TRACING);
-    let mut configuration = get_configuration().expect("Unable to get configuration");
-    configuration.database.database = uuid::Uuid::new_v4().to_string();
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:0")
-        .await
-        .expect("Binding listener");
-    let port = listener.local_addr().unwrap().port();
-    let pool = configure_database(&configuration.database).await;
-
-    let server = run(listener, pool.clone())
-        .await
-        .expect("Failed to bind address");
-
-    let _ = tokio::spawn(server.into_future());
-    TestApp {
-        address: format!("http://127.0.0.1:{}", port),
-    }
-}
