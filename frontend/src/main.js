eb0803374bb6140a32c56bf5059c923b34702b9b
Jean-Mathieu Deschenes <jm@pinkbot.com>
2025-02-22 14:28:05 -0500

    Added initial react template

---
+++ frontend/src/main.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+ReactDOM.createRoot(document.getElementById("root")).render(React.createElement(React.StrictMode, null, "POTATO"));
+//# sourceMappingURL=main.js.map
