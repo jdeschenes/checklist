777bfacaf856d567c054e80b97565adc224f82cc
Jean-Mathieu Deschenes <jm@pinkbot.com>
2025-12-26 09:11:52 -0500

    fix: Fixed eslint errors

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -6,7 +6,13 @@ import pluginQuery from "@tanstack/eslint-plugin-query";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const ignores = ["dist", "prettier.config.mjs", "vite.config.ts"];
+const ignores = [
+  "dist",
+  "eslint.config.mjs",
+  "prettier.config.mjs",
+  "vite.config.ts",
+];
+const tsFiles = ["**/*.{ts,tsx}"];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,20 +22,29 @@ export default tseslint.config(
   eslint.configs.recommended,
   reactLint.configs.flat.recommended,
   reactLint.configs.flat["jsx-runtime"],
-  ...tseslint.configs.recommendedTypeChecked,
   {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: __dirname,
-      },
-    },
     settings: {
       react: {
         version: "detect",
       },
     },
   },
-  eslintConfigPrettier,
-  ...pluginQuery.configs["flat/recommended"]
+  ...tseslint.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    files: tsFiles,
+  })),
+  {
+    files: tsFiles,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+  ...pluginQuery.configs["flat/recommended"],
+  eslintConfigPrettier
 );
